3f4307c582f3cc19475b75f9d1dd6183521ca65b
zaezero97 <xornjs1423@gmail.com>
2022-02-05 22:10:46 +0900

    프로젝트 생성

---
+++ Qnnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Qnnect
+//
+//  Created by 재영신 on 2022/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
