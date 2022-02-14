2327ea8c671a203d4da3cabc214191d93d8749e6
zaezero97 <xornjs1423@gmail.com>
2022-02-14 13:58:01 +0900

    :lipstick: Color asset + extension 추가
    Color asset + extension 추가

---
+++ Qnnect/Presentation/LoginScene/View/ProfileImageView.swift
@@ -0,0 +1,8 @@
+//
+//  profileImageView.swift
+//  Qnnect
+//
+//  Created by 재영신 on 2022/02/14.
+//
+
+import Foundation
---
+++ Qnnect/Util/Extension/UIColor+.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+.swift
+//  Qnnect
+//
+//  Created by 재영신 on 2022/02/14.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class var BLACK_121212: UIColor? { return UIColor(named: "BLACK_121212") }
+    class var WHITE_FFFFFF: UIColor? { return UIColor(named: "WHITE_FFFFFF") }
+    class var BLUE01: UIColor? { return UIColor(named: "BLUE01") }
+    class var BLUE02: UIColor? { return UIColor(named: "BLUE02") }
+    class var GRAY01: UIColor? { return UIColor(named: "GRAY01") }
+    class var GRAY02: UIColor? { return UIColor(named: "GRAY02") }
+    class var GRAY03: UIColor? { return UIColor(named: "GRAY03") }
+    class var GRAY04: UIColor? { return UIColor(named: "GRAY04") }
+    class var GRAY05: UIColor? { return UIColor(named: "GRAY05") }
+    class var GREEN01: UIColor? { return UIColor(named: "GREEN01") }
+    class var GREEN02: UIColor? { return UIColor(named: "GREEN02") }
+    class var PINK01: UIColor? { return UIColor(named: "PINK02") }
+    class var PINK02: UIColor? { return UIColor(named: "PINK02") }
+    class var PRIMARY01: UIColor? { return UIColor(named: "PRIMARY01") }
+    class var PRIMARY02: UIColor? { return UIColor(named: "PRIMARY02") }
+    class var PURPLE01: UIColor? { return UIColor(named: "PURPLE01") }
+    class var PURPLE02: UIColor? { return UIColor(named: "PURPLE02") }
+    class var PURPLE03: UIColor? { return UIColor(named: "PURPLE03") }
+    class var PURPLE04: UIColor? { return UIColor(named: "PURPLE04") }
+    class var SECONDARY01: UIColor? { return UIColor(named: "SECONDARY01") }
+    class var SECONDARY02: UIColor? { return UIColor(named: "SECONDARY02") }
+    class var SECONDARY03: UIColor? { return UIColor(named: "SECONDARY03") }
+    class var SECONDARY04: UIColor? { return UIColor(named: "SECONDARY04") }
+}
