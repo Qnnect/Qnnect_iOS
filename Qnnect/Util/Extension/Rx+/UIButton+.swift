3bae0678e88fb7c053c60513e2b530056ca72dc1
zaezero97 <xornjs1423@gmail.com>
2022-02-15 12:57:22 +0900

    :sparkles: 닉네임 입력이 유효 할 때 버튼 배경 색상 변경기능 추가
    닉네임 입력이 유효 할 때 버튼 배경 색상 변경기능 추가

--- Qnnect/Util/Extension/Rx+/UIButton+.swift
+++ Qnnect/Util/Extension/Rx+/UIButton+.swift
@@ -13,7 +13,7 @@ extension Reactive where Base: UIButton {
     public var setEnabled: Binder<Bool> {
         return Binder(self.base) { button,flag in
             button.isEnabled = flag
-            //TODO: 버튼 비활성화 STYLE 로직 추가 해야함 ex) 버튼 회색 으로 변경
+            button.backgroundColor = flag ? .p_brown : .GRAY04
         }
     }
 }
