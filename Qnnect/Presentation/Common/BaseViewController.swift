bbf05e61ae5447482d873fbc797ad125162c0f3b
zaezero97 <xornjs1423@gmail.com>
2022-02-25 16:18:53 +0900

    :sparkles: 화면터치시 키보드 내리기 추가
    화면터치시 키보드 내리기 추가

--- Qnnect/Presentation/Common/BaseViewController.swift
+++ Qnnect/Presentation/Common/BaseViewController.swift
@@ -27,4 +27,7 @@ class BaseViewController: UIViewController {
         
     }
  
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+    }
 }
