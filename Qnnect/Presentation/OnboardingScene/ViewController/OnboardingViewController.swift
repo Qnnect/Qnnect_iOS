777eac62bd0a26b2e3dffead904aec705ee698c1
zaezero97 <xornjs1423@gmail.com>
2022-03-30 19:21:02 +0900

    :lipstick: 온보딩화면 다른화면갔다가 첫페이지로 넘어오면 이전페이지 살짝보이는 부분 수정
    UIPageViewController interPageSpaice 10 으로 설정

--- Qnnect/Presentation/OnboardingScene/ViewController/OnboardingViewController.swift
+++ Qnnect/Presentation/OnboardingScene/ViewController/OnboardingViewController.swift
@@ -9,53 +9,6 @@ import UIKit
 import RxSwift
 import SnapKit
 
-enum OnboardingSceneType: CaseIterable {
-    case question
-    case connection
-    case drink
-    case stamp
-    
-    var mainTitle: String {
-        switch self {
-        case .question:
-            return "나를 알아가는 질문"
-        case .connection:
-            return "가족/친구/커플간의\nQ&A 공유를 통한 소통"
-        case .drink:
-            return "나만의 음료 만들기"
-        case .stamp:
-            return "스탬프 - 내가 만든 음료 모아보기"
-        }
-    }
-    
-    var secondaryTitle: String {
-        switch self {
-        case .question:
-            return "큐넥트 지킴이 넥트가\n여러분에게 단 하나의 질문을 드려요\nQ&A를 통해 나를 알아가고 일상을 회고해보세요!"
-        case .connection:
-            return "Q&A를 가족/친구/커플끼리 공유하여 서로에\n대해 알아가고 소통할 수 있어요!\n기본 제공 질문 외에 카페 멤버에게 하고싶은 질문을 직접 추가할 수도 있어요\n(큐넥트에서 카페란, 다이어리 그룹을 의미해요️)"
-        case .drink:
-            return "답변을 달거나 질문을 추가할수록 쌓이는\n원두 포인트를 통해\n나만의 음료를 만들어보세요"
-        case .stamp:
-            return "음료를 완성할 때마다 스탬프가 음료로 채워져요\n스탬프를 멋지게 채워보세요!"
-        }
-    }
-    
-    var image: UIImage? {
-        switch self {
-        case .question:
-            return Constants.onboardingImage1
-        case .connection:
-            return Constants.onboardingImage2
-        case .drink:
-            return Constants.onboardingImage3
-        case .stamp:
-            return Constants.onboardingImage4
-        }
-    }
-    
-}
-
 final class OnboardingViewController: UIPageViewController{
     private var pages = [UIViewController]()
     private let pageControl = UIPageControl().then {
@@ -90,7 +43,7 @@ final class OnboardingViewController: UIPageViewController{
         let vc = OnboardingViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal,
-            options: nil
+            options: [UIPageViewController.OptionsKey.interPageSpacing: 10.0]
         )
         vc.coordinator = coordinator
         vc.viewModel = viewModel
@@ -230,7 +183,6 @@ extension OnboardingViewController : UIPageViewControllerDataSource{
 
 extension OnboardingViewController : UIPageViewControllerDelegate{
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        
         guard let currentVC = pageViewController.viewControllers?.first else { return }
         guard let currentIndex = pages.firstIndex(of: currentVC) else { return }
         
