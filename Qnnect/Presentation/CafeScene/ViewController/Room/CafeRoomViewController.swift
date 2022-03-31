29637d541019059a5ae5934d4fbf9a3324c3dffe
zaezero97 <xornjs1423@gmail.com>
2022-03-31 11:37:48 +0900

    :sparkles: 카페 홈에서 음료셀이아닌 섹션 클릭 하면 우리의 카페 입장으로 변경
    카페 홈에서 음료셀이아닌 섹션 클릭 하면 우리의 카페 입장으로 변경

--- Qnnect/Presentation/CafeScene/ViewController/Room/CafeRoomViewController.swift
+++ Qnnect/Presentation/CafeScene/ViewController/Room/CafeRoomViewController.swift
@@ -185,10 +185,11 @@ final class CafeRoomViewController: BaseViewController {
                     return question
                 },
             didTapQuestionListButton: navigationQuestionListButton.rx.tap.asObservable(),
-            didTapDrinkSection: mainCollectionView.rx.itemSelected
-                        .filter { $0.section == 1 }
-                        .mapToVoid()
-                
+            didTapDrinkSection: rx.methodInvoked(#selector(didTapDrinkSection)).mapToVoid()
+//                mainCollectionView.rx.itemSelected
+//                        .filter { $0.section == 1 }
+//                        .mapToVoid()
+//
         )
         
         let output = self.viewModel.transform(from: input)
@@ -466,8 +467,11 @@ private extension CafeRoomViewController {
                 forElementKind: UICollectionView.elementKindSectionFooter,
                 at: IndexPath(row: 0, section: 1)) as? PageControlFooterView
         else { return }
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapDrinkSection))
+        decoView.addGestureRecognizer(tapGesture)
         decoView.emptyLabel.isHidden = !isEmpty
         footerView.pageControl.isHidden = isEmpty
     }
     
+    @objc func didTapDrinkSection() { }
 }
