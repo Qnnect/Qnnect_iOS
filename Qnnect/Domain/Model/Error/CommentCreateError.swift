875813a1392499848fab49ded6d73d6f04985a24
zaezero97 <xornjs1423@gmail.com>
2022-04-04 19:39:32 +0900

    :sparkles: 답변기한지난 질문 에러처리 추가
    답변기한지난 질문 에러처리 추가

--- Qnnect/Domain/Model/Error/CommentCreateError.swift
+++ Qnnect/Domain/Model/Error/CommentCreateError.swift
@@ -9,12 +9,15 @@ import Foundation
 
 enum CommentCreateError: Error {
     case notSelectDrink
+    case passedDate
     case unknown
     
     init(statusCode: Int) {
         switch statusCode {
         case 401:
             self = .notSelectDrink
+        case 423:
+            self = .passedDate
         default:
             self = .unknown
         }
@@ -24,6 +27,8 @@ enum CommentCreateError: Error {
         switch self {
         case .notSelectDrink:
             return "음료를 선택하지 않아\n답변을 할 수 없습니다."
+        case .passedDate:
+            return "질문을 답변할 수 있는 기간이 지났습니다."
         case .unknown:
             return "unknown Error"
         }
